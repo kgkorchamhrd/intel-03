dda3cde9e7006c7faadda9795fde0940d00a6e7b
jaesangjo <jo589396@naver.com>
2025-02-28 02:42:27 +0900

    class01-hw4-jo589396

---
+++ class01/homework/jojaesang/hw4_pythonHW_and_math/Medical_images_classification.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+from PIL import Image
+
+import tensorflow
+from tensorflow.keras import datasets, layers, models, Model, Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, AveragePooling2D, Flatten, Dense, Input, BatchNormalization, Concatenate, Dropout
+from tensorflow.keras.preprocessing.image import ImageDataGenerator, load_img
+
+mainDIR =os.listdir('/chest_xray')
+print(mainDIR)
+train_folder= './chest_xray/train/'
+val_folder = './chest_xray/val/'
+test_folder = './chest_xray/test/'
+# train 
+os.listdir(train_folder)
+train_n = train_folder+'NORMAL/'
+train_p = train_folder+'PNEUMONIA/'
+#Normal pic 
+print(len(os.listdir(train_n)))
+rand_norm= np.random.randint(0,len(os.listdir(train_n)))
+norm_pic = os.listdir(train_n)[rand_norm]
+print('normal picture title: ',norm_pic)
+norm_pic_address = train_n+norm_pic
+#Pneumonia
+rand_p = np.random.randint(0,len(os.listdir(train_p)))
+sic_pic = os.listdir(train_p)[rand_norm]
+sic_address = train_p+sic_pic
+print('pneumonia picture title:', sic_pic)
+
+norm_load = Image.open(norm_pic_address)
+sic_load = Image.open(sic_address)
